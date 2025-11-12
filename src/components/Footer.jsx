bd1d705e4a18f64d619af66d4519aa81d8d3d715
Muhiuddin <muhiuddin.bd.2005@gmail.com>
2025-11-12 22:52:32 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
-    return (
-        <div>
-            footer
+  return (
+    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About</a>
+        <a className="link link-hover">Contact</a>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <FaFacebook className="text-2xl" />
+          </a>
+          <a>
+            <FaTwitter className="text-2xl" />
+          </a>
+          <a>
+            <FaInstagram className="text-2xl" />
+          </a>
         </div>
-    );
+      </nav>
+      <aside>
+        <p>
+          © 2025 EcoTrack — All rights reserved. <br />
+          <span className="text-sm">
+            Accessibility and Privacy . Designed for a greener globe!
+          </span>
+        </p>
+      </aside>
+    </footer>
+  );
 };
 
 export default Footer;
